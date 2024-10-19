2614f91d9a10d03fba6271662b30f3f9b175d231
TheSquiddyLink <preston@happychaps.com>
2024-10-19 10:58:52 -0700

    Get Heartrate

--- script/main.js
+++ script/main.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+async function getHr(){
+
+    const hr = await fetch("hr/hr.txt")
+    return await hr.text()
+}
+
+async function main(){
+    console.log(await getHr())
+    await wait(500);
+    main()
+}
+
+async function wait(ms){
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+main()
